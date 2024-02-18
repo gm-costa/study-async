ad9b77b58728b135a90219959f027c061e2eba36
gm-costa <gmaia_01@hotmail.com>
2024-02-18 12:04:22 -0300

    Personaliza visualização da área administrativa

--- apostilas/admin.py
+++ apostilas/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import Apostila, ViewApostila, Tag
 
 
-admin.site.register(Apostila)
-admin.site.register(ViewApostila)
+class ApostilaAdmin(admin.ModelAdmin):
+    list_display = ['user', 'titulo', 'arquivo', 'tag']
+
+    def tag(self, obj):
+        return [tag.tag for tag in obj.tags.all()]
+
+
+class ViewApostilaAdmin(admin.ModelAdmin):
+    list_display = ['ip', 'apostila']
+    
+
+admin.site.register(Apostila, ApostilaAdmin)
+admin.site.register(ViewApostila, ViewApostilaAdmin)
 admin.site.register(Tag)
--- flashcard/admin.py
+++ flashcard/admin.py
@@ -2,10 +2,24 @@ from django.contrib import admin
 from .models import Categoria, Flashcard, Desafio, FlashcardDesafio
 
 
+class FlashcardAdmin(admin.ModelAdmin):
+    list_display = ['user', 'pergunta', 'resposta', 'categoria', 'dificuldade']
+
+
+class DesafioAdmin(admin.ModelAdmin):
+    list_display = ['user', 'titulo', 'quantidade_perguntas', 'dificuldade', 'categorias', 'flashs']
+
+    def categorias(self, obj):
+        return [cat.nome for cat in obj.categoria.all()]
+    
+    def flashs(self, obj):
+        return [flash.flashcard for flash in obj.flashcards.all()]
+
+
 class FlashcardDesafioAdmin(admin.ModelAdmin):
     list_display = ['flashcard', 'respondido', 'acertou']
 
 admin.site.register(Categoria)
-admin.site.register(Flashcard)
-admin.site.register(Desafio)
+admin.site.register(Flashcard, FlashcardAdmin)
+admin.site.register(Desafio, DesafioAdmin)
 admin.site.register(FlashcardDesafio, FlashcardDesafioAdmin)
